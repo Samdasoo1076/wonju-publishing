2b21b6d59059efa4a5c62eb1c750c7810dc23ed6
조찬기 <isuda7@ucomp.co.kr>
2024-10-25 14:13:58 +0900

    인크루드 처리 수정

--- assets/js/ui/ui.publish.js
+++ assets/js/ui/ui.publish.js
@@ -4,12 +4,16 @@
 	- @ Init	    : 초기실행
 -------------------------------------------------------------------*/
 $(function(){
-	$(".header").load("../../html/layout/include_header.html", function(){
+	$(".header").each(function(){
+		$(this).load("../../html/layout/include_header.html", function(){
 			console.log('[INFO] 퍼블리싱 header.html 인클루드 완료');
 			initUI();
-	});
-	$(".footer").load("../../html/layout/include_footer.html", function(){
-			console.log('[INFO] 퍼블리싱 footer.html 인클루드 완료');
-			initUI();
+		});
+	})
+	$(".footer").each(function(){
+		$(this).load("../../html/layout/include_footer.html", function(){
+				console.log('[INFO] 퍼블리싱 footer.html 인클루드 완료');
+				initUI();
+		});
 	});
 });
