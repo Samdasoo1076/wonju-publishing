5a3220aa8595e4fd058326ff4513fa8f0a4dbb19
yurimkeem <yrkeem@ucomp.co.kr>
2024-11-11 18:32:06 +0900

    헤더 구현

--- assets/js/ui/ui.publish.js
+++ assets/js/ui/ui.publish.js
@@ -16,5 +16,18 @@ $(function(){
 				initUI();
 		});
 	});
+	
+	
+	/* 전체메뉴 */
+	$('.header .btn-gnb').click(function(event) {
+        event.preventDefault(); // a 태그 기본 동작 방지
+        $('.header .area-siteMap').show();
+    });
+    
+    // .header 내의 .area-siteMap 안의 .menuClose 클릭 시 .area-siteMap 숨기기
+    $('.header .area-siteMap .menuClose').click(function(event) {
+        event.preventDefault(); // a 태그 기본 동작 방지
+        $('.header .area-siteMap').hide();
+    });
 });
 
